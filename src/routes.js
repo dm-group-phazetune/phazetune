ad95e65c0c9290bfce58cddb68dd65760ecff9c0
Tramy Nguyen <tramynn2@gmail.com>
2019-10-21 17:06:54 -0500

    set up routes

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Landing from "./Components/Landing/Landing";
+import Newsfeed from "./Components/Newsfeed/Newsfeed";
+import Explore from "./Components/Explore/Explore";
+import Profile from "./Components/Profile/Profile";
+import Chat from "./Components/Chat/Chat";
+
+export default (
+  <Switch>
+    <Route component={Landing} exact path="/" />
+    <Route component={Newsfeed} path="/newsfeed" />
+    <Route component={Explore} path="/explore" />
+    <Route component={Profile} path="/profile" />
+    <Route component={Chat} path="/chat" />
+    <Route
+      render={() => {
+        return <h1>404 Page Not Found.</h1>;
+      }}
+    />
+  </Switch>
+);
