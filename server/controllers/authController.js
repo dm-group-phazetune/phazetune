4e203e51b9ea6524ff3454a017f1e58849601e15
Tramy Nguyen <tramynn2@gmail.com>
2019-10-20 19:51:33 -0500

    set up auth ctrler and landing boilerplate

---
+++ server/controllers/authController.js
@@ -0,0 +1,18 @@
+// tn - Get User
+async function getUser() {}
+
+// tn - User Register
+async function register() {}
+
+// tn - User Login
+async function login() {}
+
+// tn - User Logout
+async function logout() {}
+
+module.exports = {
+  getUser,
+  register,
+  login,
+  logout
+};
