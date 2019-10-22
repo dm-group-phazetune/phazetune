c2e32571a06b14fa7c31f5c34ba9860dfe3ae00d
Tramy Nguyen <tramynn2@gmail.com>
2019-10-22 16:59:46 -0500

    login front end works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import routes from "./routes";
+import Header from "./Components/Header/Header";
 import "./styles/main.scss";
-import AudioUpload from './Components/FireAudioUpload/AudioUpload'
+import AudioUpload from "./Components/FireAudioUpload/AudioUpload";
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <AudioUpload />
       <h1>{routes}</h1>
     </div>
@@ -12,4 +15,3 @@ function App() {
 }
 
 export default App;
-
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,163 @@
 import React, { Component } from "react";
-import { getSession, logoutUser } from "../../redux/reducers/authReducer";
+import {
+  getSession,
+  loginUser,
+  registerUser,
+  logoutUser
+} from "../../redux/reducers/authReducer";
 import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 class Header extends Component {
   constructor() {
     super();
-    this.state = "";
+    this.state = {
+      login: false,
+      register: false,
+      username: "",
+      password: "",
+      first_name: "",
+      last_name: "",
+      location: ""
+    };
   }
 
   componentDidMount() {
     this.props.getSession();
   }
 
+  openLogin = () => {
+    this.setState({ login: true });
+  };
+
+  openRegister = () => {
+    this.setState({ register: true });
+  };
+
+  handleLogin = e => {
+    e.preventDefault();
+    // console.log("hit");
+    const { username, password } = this.state;
+    const { loginUser } = this.props;
+    // console.log(username, password);
+    // console.log(loginUser({ username, password }));
+    loginUser({ username, password });
+    this.setState({ login: false });
+  };
+
+  handleRegister = e => {
+    e.preventDefault();
+    this.setState({ register: false });
+  };
+
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    if (this.props.user_id) {
+      return <Redirect to="/newsfeed" />;
+    }
     return (
       <div>
         <div>PHAZETUNE</div>
-        <div>LOGIN</div>
+        <button onClick={this.openLogin}>LOGIN</button>
+        <Dialog open={this.state.login}>
+          <DialogTitle>Welcome Back</DialogTitle>
+          <DialogContent>
+            <form>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <label>username:</label>
+                    </td>
+                    <td>
+                      <input
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleInput}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label>password:</label>
+                    </td>
+                    <td>
+                      <input
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleInput}
+                      />
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <button type="submit" onClick={this.handleLogin}>
+                LOG IN
+              </button>
+            </form>
+          </DialogContent>
+        </Dialog>
+        <button name="register" type="button" onClick={this.openRegister}>
+          REGISTER
+        </button>
+        <Dialog open={this.state.register}>
+          <DialogTitle>Sign Up</DialogTitle>
+          <DialogContent>
+            <form>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <label>First Name:</label>
+                    </td>
+                    <td>
+                      <input placeholder="" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label>Last Name:</label>
+                    </td>
+                    <td>
+                      <input placeholder="" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label>Username:</label>
+                    </td>
+                    <td>
+                      <input placeholder="" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label>Password:</label>
+                    </td>
+                    <td>
+                      <input placeholder="" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label>Location:</label>
+                    </td>
+                    <td>
+                      <input placeholder="" />
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <button onClick={this.handleRegister}>Register</button>
+            </form>
+          </DialogContent>
+        </Dialog>
       </div>
     );
   }
@@ -33,6 +174,8 @@ export default connect(
   mapStateToProps,
   {
     getSession,
+    loginUser,
+    registerUser,
     logoutUser
   }
 )(Header);
