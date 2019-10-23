7c79743462b6afe25a4b9b7043a48b9cc763caf7
Naji3000 <najiali3000@gmail.com>
2019-10-23 13:47:10 -0500

    Naji fix dB

--- db/prof/editUserProfile.sql
+++ db/prof/editUserProfile.sql
@@ -2,8 +2,8 @@ UPDATE users
 SET 
 first_name = $1,
 last_name = $2,
-location = $3,
+city = $3,
 bio = $4
 WHERE user_id = $5;
 
-SELECT first_name, last_name, location, bio FROM users WHERE user_id = $5
+SELECT first_name, last_name, city, bio FROM users WHERE user_id = $5
