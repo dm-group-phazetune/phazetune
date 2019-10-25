6555c323435c16a78dfd9c238ecfb10489020de9
Tramy Nguyen <tramynn2@gmail.com>
2019-10-25 12:38:25 -0500

    styled footernav

--- src/Components/FooterNav/FooterNav.js
+++ src/Components/FooterNav/FooterNav.js
@@ -1,34 +1,79 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter, Link } from "react-router-dom";
 import { getSession } from "../../redux/reducers/authReducer";
-import { FontAwesomeIcon } from "@fortawesome/fontawesome-svg-core";
-import { faHome, faSearch, faPlusCircle, faCommentDots,  } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faSearch,
+  faPlusCircle,
+  faCommentDots,
+  faUserCircle
+} from "@fortawesome/free-solid-svg-icons";
 
-export default class FooterNav extends Component {
+class FooterNav extends Component {
   componentDidMount() {
     this.props.getSession();
   }
 
   render() {
     return (
-      <div className="FooterNav-container">
-        <div className="Newsfeed-container"></div>
-        <div className="Explore-container"></div>
-        <div className="AddPost-container"></div>
-        <div className="Chat-container"></div>
-        <div className="Profile-container"></div>
+      <div>
+        {this.props.location.pathname !== "/" ? (
+          <div className="FooterNav-container">
+            <Link
+              to="/newsfeed"
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <div className="Footernav">
+                <FontAwesomeIcon icon={faHome} size="2x" />
+              </div>
+            </Link>
+            <Link
+              to="/explore"
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <div className="Footernav">
+                <FontAwesomeIcon icon={faSearch} size="2x" />
+              </div>
+            </Link>
+            <div className="Footernav">
+              <FontAwesomeIcon icon={faPlusCircle} size="2x" />
+            </div>
+            <Link
+              to="/chat"
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <div className="Footernav">
+                <FontAwesomeIcon icon={faCommentDots} size="2x" />
+              </div>
+            </Link>
+            <Link
+              to="/profile"
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
+              <div className="Footernav">
+                <FontAwesomeIcon icon={faUserCircle} size="2x" />
+              </div>
+            </Link>
+          </div>
+        ) : null}
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = reduxState => {
   return {
     user_id: reduxState.authReducer.user_id
-  }
-}
+  };
+};
 
-export default withRouter(connect(mapStateToProps, {
-  getSession
-})(FooterNav));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    {
+      getSession
+    }
+  )(FooterNav)
+);
