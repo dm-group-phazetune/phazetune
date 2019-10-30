4b08d75f6630c7c4b26e07e4110402e5d6913468
Naji3000 <najiali3000@gmail.com>
2019-10-30 15:24:45 -0500

    Naji update & merge

--- src/_test_/NajiComponent.test.js
+++ src/_test_/NajiComponent.test.js
@@ -4,6 +4,7 @@ import {Provider} from "react-redux";
 import ConfigureMockStore from "redux-mock-store";
 import Adapter from "enzyme-adapter-react-16";
 
+import Profile from '../Components/Profile/Profile'
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -11,3 +12,15 @@ const mockStore = ConfigureMockStore();
 const store = mockStore({
     postReducer:{}
 });
+
+describe("component test", () => {
+    test('can render all properly', () => {
+        expect (
+            shallow(
+                <Provider store ={store} >
+                    <Profile />
+                </Provider>
+            )
+        )
+    })
+})
