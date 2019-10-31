3f58bc98e985ca348ed4bc9e98e2dc6ba449219d
Naji3000 <najiali3000@gmail.com>
2019-10-30 22:24:58 -0500

    Naji Component test

--- src/_test_/NajiComponent.test.js
+++ src/_test_/NajiComponent.test.js
@@ -3,8 +3,15 @@ import Enzyme, {shallow} from 'enzyme'
 import {Provider} from "react-redux";
 import ConfigureMockStore from "redux-mock-store";
 import Adapter from "enzyme-adapter-react-16";
+import ReactDOM from 'react-dom'
+import { act } from "react-dom/test-utils"
 
-import Profile from '../Components/Profile/Profile'
+
+import AudioPlayer from '../Components/FooterNav/AudioPlayer'
+import Explore from '../Components/Explore/Explore'
+import AudioUpload from '../Components/FooterNav/AudioUpload';
+import ArtistChat from '../Components/ArtistsChat/ArtistsChat'
+import ChatNav from '../Components/ChatNav/ChatNav'
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -18,9 +25,56 @@ describe("component test", () => {
         expect (
             shallow(
                 <Provider store ={store} >
-                    <Profile />
+                    <Explore/>
                 </Provider>
             )
         )
     })
+    test('can render Properly', () => {
+        expect(
+            shallow(
+                <Provider store ={store} >
+                    <AudioPlayer/>
+                </Provider>
+
+            )
+        )
+    })
+    test('can render Properly', () => {
+        expect(
+            shallow(
+                <Provider store ={store} >
+                    <AudioUpload/>
+                </Provider>
+            )
+        )
+    })
+    test('can render Properly', () => {
+        expect(
+            shallow(
+                <Provider store ={store} >
+                    <ArtistChat />
+                </Provider>
+            )
+        )
+    })
+    test('can render Properly', () => {
+        expect(
+            shallow(
+                <Provider store ={store} >
+                    <ChatNav />
+                </Provider>
+            )
+        )
+    })
+    // test('can render Properly', () => {
+    //     expect(
+    //         shallow(
+    //             <Provider store ={store} >
+    //                 <FireBase />
+    //             </Provider>
+    //         )
+    //     )
+    // })
 })
+
